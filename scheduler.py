a42008af6155711ad187aa5002e9e6158b740da4
shubham.luxkar <shubham.luxkar@tmotions.com>
2024-12-10 11:49:48 +0530

    Bug Fixes

--- scheduler.py
+++ scheduler.py
@@ -1,27 +1,32 @@
-import schedule
-import time
+import os
 import subprocess
 import sys
-import os
-from watchdog.observers import Observer
+import time
+
+import schedule
 from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
 
 class PdfFolderHandler(FileSystemEventHandler):
     def on_created(self, event):
         if event.is_directory:
             return None
-        if event.src_path.endswith('.pdf'):
+        if event.src_path.endswith(".pdf"):
             run_main()
 
+
 def run_email_fetch_test():
-    script_path = os.path.join('email_tools', 'email_fetch.py')
+    script_path = os.path.join("email_tools", "email_fetch.py")
     subprocess.Popen([sys.executable, script_path])
 
+
 def run_main():
-    subprocess.Popen([sys.executable, 'main.py'])
+    subprocess.Popen([sys.executable, "main.py"])
+
 
 def watch_mail_pdfs_folder():
-    path = './mailPdfs'
+    path = "./mailPdfs"
     event_handler = PdfFolderHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
@@ -33,10 +38,12 @@ def watch_mail_pdfs_folder():
         observer.stop()
     observer.join()
 
+
 schedule.every(10).seconds.do(run_email_fetch_test)
 
 # Run the watchdog in a separate thread
 import threading
+
 thread = threading.Thread(target=watch_mail_pdfs_folder)
 thread.daemon = True  # Set as daemon so it exits when main thread exits
 thread.start()
